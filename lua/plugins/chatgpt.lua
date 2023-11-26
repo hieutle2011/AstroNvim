d467e1d8f5bba9cc9a94237f3fc013c089a037a1
Hieu Le <hieu.trung@yolo.com>
2023-11-26 20:58:41 +0200

    chatgpt plugin

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,13 @@
+return {
+  "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://private/openai/apikey --no-newline"
+    }) end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+}
